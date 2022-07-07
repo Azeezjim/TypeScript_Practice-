96d4675b9327fea6cec845529ebc38f6664ca6bd
Azeezjim <azeexjim@gmail.com>
2022-07-06 15:53:55 -1100

    Mytypescript first commit

--- Script_2/sandbox.js
+++ Script_2/sandbox.js
@@ -5,3 +5,11 @@ console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
+var myName = "Azeezjim";
+myName = "Abdul Azeez";
+var age = 23;
+age = 24;
+var circ = function (diametar) {
+    diametar * Math.PI;
+};
+console.log(circ(5.5));
--- Script_2/sandbox.ts
+++ Script_2/sandbox.ts
@@ -8,3 +8,18 @@ console.log(inputs)
 inputs.forEach(input => {
   console.log(input)
 })
+
+let myName = "Azeezjim";
+
+myName = "Abdul Azeez"
+
+let age = 23;
+
+age = 24;
+
+const circ = (diametar : number) => {
+  diametar * Math.PI
+}
+
+
+console.log(circ(5.5))
