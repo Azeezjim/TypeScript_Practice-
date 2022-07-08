c7f1b487559e689ae6043ffd046f2427ccb10b98
Azeezjim <azeexjim@gmail.com>
2022-07-07 16:10:42 -1100

    Mytypescript first commit

--- Script_2/sandbox.js
+++ Script_2/sandbox.js
@@ -7,12 +7,25 @@ inputs.forEach(function (input) {
 });
 var myName = "Azeezjim";
 myName = "Abdul Azeez";
-var age = 23;
-age = 24;
+// let age = 23;
+// age = 24;
 var circ = function (diametar) {
     diametar * Math.PI;
 };
 console.log(circ(5.5));
 // leason 4
-var names = ['Azeezjim', 'Azeez', ' jim'];
+var names = ['Azeezjim', 'Azeez', 'jim'];
 names.push('Garuba');
+// names.push (3)
+// leason 
+var age; // age can onlr be a type of number 
+age = 30;
+// we can also specify the data type an arrray or object should have while innitializing them 
+var objectName;
+objectName = ['Lateefa'];
+objectName.push('Jamilah');
+// union Type 
+var mixed = [];
+mixed.push(25);
+mixed.push("Ibnu Baz");
+console.log(mixed);
--- Script_2/sandbox.ts
+++ Script_2/sandbox.ts
@@ -13,9 +13,9 @@ let myName = "Azeezjim";
 
 myName = "Abdul Azeez"
 
-let age = 23;
+// let age = 23;
 
-age = 24;
+// age = 24;
 
 const circ = (diametar : number) => {
   diametar * Math.PI
@@ -24,6 +24,28 @@ console.log(circ(5.5))
 
 // leason 4
 
-let names = ['Azeezjim', 'Azeez', ' jim']
+let names = ['Azeezjim', 'Azeez', 'jim']
 
 names.push ('Garuba')
+// names.push (3)
+
+// leason 
+
+let age: number; // age can onlr be a type of number 
+age = 30
+
+// we can also specify the data type an arrray or object should have while innitializing them 
+
+let objectName: string[]
+
+objectName = ['Lateefa']
+
+objectName.push('Jamilah')
+
+// union Type 
+
+let  mixed: (string|number)[] =[]
+
+mixed.push(25)
+mixed.push("Ibnu Baz")
+console.log(mixed)
